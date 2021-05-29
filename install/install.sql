5031002c577e6bfac18738610e7c2a1ac16d089f
xmcword <69714119+xmcword@users.noreply.github.com>
2021-05-29 11:40:17 +0800

    Add files via upload

---
+++ install/install.sql
@@ -0,0 +1,103 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `passwords` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `appid` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `apppwd` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `mailu` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `mailp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `token` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '', '', '', '', '63824f9e0afd4ffb18a38cc836d31f17');
+
+-- ----------------------------
+-- Table structure for config
+-- ----------------------------
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE `config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题',
+  `tail` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题尾巴',
+  `keywords` varchar(99) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '关键词',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '简介',
+  `gg` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '网站公告',
+  `qq` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'qq',
+  `background` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '背景图片',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '网站配置' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of config
+-- ----------------------------
+INSERT INTO `config` VALUES (1, '个人发卡网', '商业版3.5', '个人,发卡网,自助提卡,卡密', '个人发卡网是收款免签约自助提卡系统,安全、稳定、不加收任何费用、直接到账、一站式售卡系统。', '欢迎使用2019全新个人发卡网商业版，官网:<a href=\"http://km.edlm.cn/\">km.edlm.cn</a> ！', '100000', 'images/bg-01.jpg');
+
+-- ----------------------------
+-- Table structure for kms
+-- ----------------------------
+DROP TABLE IF EXISTS `kms`;
+CREATE TABLE `kms`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `km` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '卡密',
+  `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '购买时间',
+  `spid` int(11) NOT NULL COMMENT '所属商品',
+  `order` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '所属订单',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 62 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '卡密数据' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for lists
+-- ----------------------------
+DROP TABLE IF EXISTS `lists`;
+CREATE TABLE `lists`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商品名字',
+  `mode` varchar(999) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商品说明',
+  `money` decimal(50, 2) NOT NULL COMMENT '商品价格',
+  `type` int(11) NOT NULL COMMENT '所属分类',
+  `s` int(1) NULL DEFAULT 1 COMMENT '开启状态',
+  `or` int(11) NULL DEFAULT 1 COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '商品列表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `spid` int(11) NULL DEFAULT NULL COMMENT '商品ID',
+  `order` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单号',
+  `money` decimal(50, 2) NOT NULL COMMENT '订单价格',
+  `time` int(11) NOT NULL COMMENT '订单数量',
+  `win_order` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '支付单号',
+  `type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '支付方式',
+  `pwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '联系方式',
+  `stime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 27 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for type
+-- ----------------------------
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE `type`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `s` int(1) NULL DEFAULT 1,
+  `or` int(11) NULL DEFAULT 1 COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
